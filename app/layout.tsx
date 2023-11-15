b0725b88b5df2e98d4a714ac3923602288ab93ec
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-15 22:34:04 +1300

    🐎 ci(changed global font):

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang={LANG.en}>
-      <body className={kenia.className}>
-        <main className='max-w-10xl mx-auto'>
+      <body className={chathura.className}>
+        <main className='max-w-10xl'>
           <Navbar />
           {children}
         </main>
