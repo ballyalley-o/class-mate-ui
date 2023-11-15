06d85a3989309efd656c9648ff969156b0a4773b
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-15 22:38:19 +1300

    🐎 ci(navbar changes):

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import * as _ from '@assets/styles/navbar'
 import NavBrand from './NavBrand'
 
@@ -9,6 +10,7 @@ const Navbar = () => {
       <nav className={_.StyledNav}>
         <div className={_.StyledIconsContainer}>
           <NavBrand />
+          <div className={_.StyledIconsContainer}></div>
         </div>
       </nav>
     </header>
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Navbar } from '@components/Navbar/Navbar'
+export { default as NavSidebar } from '@components/Navbar/NavSidebar'
