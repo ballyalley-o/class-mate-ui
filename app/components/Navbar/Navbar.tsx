67aa6a5108a61b6e69828458d88844d709eb30e9
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-20 21:34:30 +1300

    🎉 init(init navbar components):

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import * as _ from '@assets/styles/navbar'
 import NavBrand from './NavBrand'
+import NavMenu from './NavMenu'
 
 const Navbar = () => {
   return (
@@ -10,7 +11,10 @@ const Navbar = () => {
       <nav className={_.StyledNav}>
         <div className={_.StyledIconsContainer}>
           <NavBrand />
-          <div className={_.StyledIconsContainer}></div>
+
+          <div className={_.StyledIconsContainer}>
+            <NavMenu />
+          </div>
         </div>
       </nav>
     </header>
