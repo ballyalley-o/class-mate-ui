57163f06b79aa9b226b37370ded46a308a06b3cf
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-20 21:35:47 +1300

    🎉 init(nav menu):

---
+++ app/components/Navbar/NavMenu.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavMenu = () => {
+  return (
+    <div className='container-home bg-inherit'>
+      <h1 className='text-4xl'></h1>
+    </div>
+  )
+}
+
+export default NavMenu
