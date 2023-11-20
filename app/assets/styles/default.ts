66694b71990f079260e764b445444de21371e151
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-20 21:35:07 +1300

    🎉 init(home):

---
+++ app/assets/styles/default.ts
@@ -0,0 +1,3 @@
+import classNames from 'classnames'
+
+export const StyledDashboardH1 = classNames('hero-title-daashbord text-8xl')
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import Image from 'next/image'
+import { def } from '@assets/styles'
 import { NavSidebar } from '@components'
 import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
     <>
-      <section className='px-6 md:px-20 py-24'>
-        <div className='text-4xl'>
+      <section className='px-6 md:px-20 py-24 align-middle border border-red-500'>
+        <div className='text-4xl container'>
           <NavSidebar />
+        </div>
+        <div className='flex flex-col justify-center items-end'>
+          <h1 className={def.StyledDashboardH1}> "...stay curious"</h1>
         </div>
       </section>
     </>
