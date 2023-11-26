1c3ccfdc4fd7fe08bcde439733776856d6c1395b
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-27 02:43:18 +1300

    🎉 init(About):

--- app/components/index.ts
+++ app/components/index.ts
@@ -4,3 +4,9 @@ export { default as NavSidebar } from '@components/Navbar/NavSidebar'
 export { default as Footer } from '@components/Footer/Footer'
 // @button
 export { default as Button } from '@components/Button/Button'
+// @about
+export { default as About } from '@components/About/About'
+// @hero
+export { default as Hero } from '@components/Hero/Hero'
+// @exercises
+export { default as Exercises } from '@components/Exercises/Exercises'
