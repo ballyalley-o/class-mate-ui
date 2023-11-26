47e026f48ef0c366e1cecbeff00c4c194f61581a
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-27 02:13:16 +1300

    added button on navbar

--- app/components/Navbar/NavBrand.tsx
+++ app/components/Navbar/NavBrand.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
-import Image from 'next/link'
+import Image from 'next/image'
+import { GLOBAL } from '@config'
 import * as _ from '@assets/styles/navbar'
+import { NAV } from '@constants'
 
 const NavBrand = () => {
   return (
     <Link href='/' className={_.StyledLink}>
-      <h1 className={_.StyledTextLogo}> ClassMate</h1>
+      <Image src={GLOBAL.brand.logo} alt='logo' width={200} height={40} />
     </Link>
   )
 }
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -2,21 +2,45 @@ import Image from 'next/image'
 import Link from 'next/link'
 import * as _ from '@assets/styles/navbar'
 import NavBrand from './NavBrand'
-import NavMenu from './NavMenu'
+import { Button } from '@components'
+import { NAV } from '@constants'
 
 const Navbar = () => {
   return (
-    <header className={_.StyledHeader}>
-      <nav className={_.StyledNav}>
-        <div className={_.StyledIconsContainer}>
-          <NavBrand />
+    <nav className={_.StyledNav}>
+      <NavBrand />
+      <ul className='hidden h-full gap-12 lg:flex'>
+        {NAV.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className='light-14 hover:regular-18 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all ease-in-out'
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
 
-          <div className={_.StyledIconsContainer}>
-            <NavMenu />
-          </div>
-        </div>
-      </nav>
-    </header>
+      <div className='lg:flexCenter hidden'>
+        <Button
+          title='SIGN IN'
+          icon='rocket.svg'
+          type='button'
+          variant='btn_dark_green'
+          w={18}
+          h={18}
+          // className='inline-block cursor'
+        />
+      </div>
+
+      <Image
+        src='/assets/svg/menu-sm.svg'
+        alt='menu-mobile'
+        width={32}
+        height={32}
+        className='inline-block cursor-pointer lg:hidden'
+      />
+    </nav>
   )
 }
 
