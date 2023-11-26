c92406daba7b6e636d1aa37c493dd1dd541bdae7
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-26 23:41:41 +1300

    nav constants

---
+++ app/constants/nav.ts
@@ -0,0 +1,34 @@
+const NAV = [
+  {
+    href: '/',
+    key: 'home',
+    label: 'Home',
+  },
+  {
+    href: '/students',
+    key: 'students',
+    label: 'Students',
+  },
+  {
+    href: '/progress',
+    key: 'progress',
+    label: 'Progress',
+  },
+  {
+    href: '/exercises',
+    key: 'exercises',
+    label: 'Exercises',
+  },
+  {
+    href: '/about',
+    key: 'about',
+    label: 'About',
+  },
+  {
+    href: '/admin',
+    key: 'admin',
+    label: 'Admin',
+  },
+]
+
+export default NAV
