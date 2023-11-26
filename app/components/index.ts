27130f3dc357dc021f6aefee26b0d1281a361a43
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-26 23:44:39 +1300

    footer export

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Navbar } from '@components/Navbar/Navbar'
 export { default as NavSidebar } from '@components/Navbar/NavSidebar'
+// @footer
+export { default as Footer } from '@components/Footer/Footer'
