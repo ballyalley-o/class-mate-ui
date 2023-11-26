92faf25c55672f434adb76b053dd0a15f7458e50
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-27 02:12:26 +1300

    removed sidebar (for now)

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,6 @@ export default function Home() {
   return (
     <>
       <section className='px-6 md:px-20 py-24 align-middle border border-red-500'>
-        <div className='text-4xl container'>
-          <NavSidebar />
-        </div>
         <div className='flex flex-col justify-center items-end'>
           <h1 className={def.StyledDashboardH1}> "...stay curious"</h1>
         </div>
