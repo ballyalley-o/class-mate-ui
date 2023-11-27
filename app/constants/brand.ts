59ee7ad091265eaf9afa48eb09b50b1de07ff670
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-28 02:26:22 +1300

    🐎 ci(brand constants):

---
+++ app/constants/brand.ts
@@ -0,0 +1,9 @@
+const BRAND = {
+  title: 'ClassMate',
+  hero_title: 'ASSISTANT IN LEARNING',
+  description: `An aide proficiently supports the learning process by providing
+          instructional assistance and educational guidance, ensuring a
+          comprehensive and effective educational experience.`,
+}
+
+export default BRAND
