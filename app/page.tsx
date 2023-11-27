c09d355184fdc90407193950daa9f509783879ee
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-11-28 02:24:20 +1300

    🦄 refactor(hero in page):

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 import Image from 'next/image'
 import { def } from '@assets/styles'
-import { NavSidebar } from '@components'
+import { Hero } from '@components'
 import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
     <>
-      <section className='px-6 md:px-20 py-24 align-middle border border-red-500'>
-        <div className='flex flex-col justify-center items-end'>
-          <h1 className={def.StyledDashboardH1}> "...stay curious"</h1>
-        </div>
-      </section>
+      <Hero />
     </>
   )
 }
