822be841cce330df649645d8428ee651f9fab212
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-04 22:03:14 +1300

    rename class

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -7,7 +7,7 @@ import { BRAND } from '@constants'
 const Hero = () => {
   return (
     <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
-      <div className='hero-map' />
+      <div className='hero-card' />
 
       <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
         {/* <Image
