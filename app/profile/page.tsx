36a0cd1ba0084253f99efab8b87580d6b5296a19
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-05 01:24:08 +1300

    containers for sign-in, sign-up, and profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,13 +1,13 @@
-import { SignUp } from '@clerk/nextjs'
+import { UserProfile } from '@clerk/nextjs'
 
-const SignUpPage = () => {
+const ProfilePage = () => {
   return (
     <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
       <div className='padding-container max-container w-full pb-24'>
-        <SignUp />
+        <UserProfile />
       </div>
     </section>
   )
 }
 
-export default SignUpPage
+export default ProfilePage
--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,9 +2,11 @@ import { SignIn } from '@clerk/nextjs'
 
 const SignInPage = () => {
   return (
-    <>
-      <SignIn />
-    </>
+    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
+      <div className='padding-container max-container w-full pb-24'>
+        <SignIn />
+      </div>
+    </section>
   )
 }
 
