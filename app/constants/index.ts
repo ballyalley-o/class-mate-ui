c309b2bce85d0952cbabc4c02966dddd3a4b7572
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-09 23:44:30 +1300

    🐎 ci(projects constants):

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,2 +1,3 @@
 export { default as NAV } from '@constants/nav'
 export { default as BRAND } from '@constants/brand'
+export { default as PROJECTS } from '@constants/projects'
---
+++ app/constants/projects.ts
@@ -0,0 +1,44 @@
+const PROJECTS = [
+  {
+    project_name: 'Mini Project 1',
+    topic: 'Using API',
+    icon: 'mini-project-1',
+    variant: 'white',
+    description:
+      'The purpose of the Mini Project is to reinforce skills that have been covered in recent modules. Select a data set and analyse it using the techniques learned in recent modules. Prepare and present a 10-minute presentation in class.',
+  },
+  {
+    project_name: 'Mini Project 2',
+    topic: 'React',
+    icon: '/',
+    variant: 'white',
+    description:
+      'Create a dynamic and interactive frontend Application that uses the core principles of React.',
+    instructions: [
+      {
+        1: 'Decide on what you work on',
+      },
+      {
+        2: 'Write down your objectives in the file objectives.md , in the root directory of this repository.',
+      },
+    ],
+  },
+  {
+    project_name: 'Mini Project 3',
+    topic: 'Back-end',
+    icon: '/',
+    variant: 'white',
+    description:
+      'The purpose of the Mini Project is to reinforce skills that have been covered in recent modules. Select a data set and analyse it using the techniques learned in recent modules. Prepare and present a 10-minute presentation in class.',
+  },
+  {
+    project_name: 'Capstone',
+    topic: 'Capstone',
+    icon: '/',
+    variant: 'white',
+    description:
+      'The purpose of the Mini Project is to reinforce skills that have been covered in recent modules. Select a data set and analyse it using the techniques learned in recent modules. Prepare and present a 10-minute presentation in class.',
+  },
+]
+
+export default PROJECTS
