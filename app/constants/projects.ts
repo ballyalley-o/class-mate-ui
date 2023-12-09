985e2872c98efb53c67c6ca0a60aa9ff3b8fc9e2
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-10 03:39:46 +1300

    projects constants

--- app/constants/projects.ts
+++ app/constants/projects.ts
@@ -3,15 +3,15 @@ const PROJECTS = [
     project_name: 'Mini Project 1',
     topic: 'Using API',
     icon: 'mini-project-1',
-    variant: 'white',
+    variant: 'bg-white',
     description:
       'The purpose of the Mini Project is to reinforce skills that have been covered in recent modules. Select a data set and analyse it using the techniques learned in recent modules. Prepare and present a 10-minute presentation in class.',
   },
   {
     project_name: 'Mini Project 2',
     topic: 'React',
-    icon: '/',
-    variant: 'white',
+    icon: 'mini-project-2',
+    variant: 'bg-white',
     description:
       'Create a dynamic and interactive frontend Application that uses the core principles of React.',
     instructions: [
@@ -26,16 +26,16 @@ const PROJECTS = [
   {
     project_name: 'Mini Project 3',
     topic: 'Back-end',
-    icon: '/',
-    variant: 'white',
+    icon: 'mini-project-3',
+    variant: 'bg-white',
     description:
       'The purpose of the Mini Project is to reinforce skills that have been covered in recent modules. Select a data set and analyse it using the techniques learned in recent modules. Prepare and present a 10-minute presentation in class.',
   },
   {
     project_name: 'Capstone',
     topic: 'Capstone',
-    icon: '/',
-    variant: 'white',
+    icon: 'capstone',
+    variant: 'bg-primary-blue',
     description:
       'The purpose of the Mini Project is to reinforce skills that have been covered in recent modules. Select a data set and analyse it using the techniques learned in recent modules. Prepare and present a 10-minute presentation in class.',
   },
