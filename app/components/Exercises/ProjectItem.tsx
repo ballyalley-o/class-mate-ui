3b43cef7d107a47e9a49878cb716e3330f4b2442
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-10 15:18:51 +1300

    added project name to project item component

--- app/components/Exercises/ProjectItem.tsx
+++ app/components/Exercises/ProjectItem.tsx
@@ -18,14 +18,20 @@ const ProjectItem = ({
 }: ProjectItem) => {
   return (
     <li className='flex w-full flex-1 flex-col items-start'>
-      <div className={`rounded-xl p-2 lg:p-3 ${variant}`}>
-        <Image
-          src={`/assets/svg/${icon}.svg`}
-          alt={icon}
-          width={28}
-          height={28}
-        />
+      <div className='flex items-center'>
+        <div className={`rounded-xl p-2 lg:p-3 ${variant} mr-5`}>
+          <Image
+            src={`/assets/svg/${icon}.svg`}
+            alt={icon}
+            width={28}
+            height={28}
+          />
+        </div>
+        <span className='mr-4'>
+          <p className='regular-14'>{project_name}</p>
+        </span>
       </div>
+
       <h2 className='bold-20 lg:bold-32 mt-5 capitalize'>{topic}</h2>
       <p className='regular-16 mt- text-gray-30 lg:mt-[30px] lg:bg-none'>
         {description}
