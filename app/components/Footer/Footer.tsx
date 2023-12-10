6a5a680d9d4612b9124797627de23ac8cf07482a
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-10 15:18:19 +1300

    footer column

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,7 +1,77 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { GLOBAL } from '@config'
+import { FOOTER, SOCIALS, CONTACT_US } from '@constants'
+import { FooterColumn } from '@components'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className='flexCenter mb-24 mt-24'>
+      <div className='padding-container max-container flex w-full flex-col gap-14'>
+        <div className='flex flex-col items-start justify-center gap-[10%] md:flex-row'>
+          <Link href='/' className='mr-20'>
+            <Image
+              src={GLOBAL.brand.logo_secondary}
+              alt='brand'
+              width={120}
+              height={50}
+            />
+          </Link>
+          <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
+            {FOOTER.map((column) => (
+              <FooterColumn title={column.title}>
+                <ul className='regular-14 flex flex-col gap-4 text-gray-30'>
+                  {column.links.map((link) => (
+                    <Link href='/' key={link}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            ))}
+            <div className='flex flex-col gap-5'>
+              <FooterColumn title={CONTACT_US.title}>
+                {CONTACT_US.links.map((link) => (
+                  <Link
+                    href='/'
+                    key={link.label}
+                    className='flex gap-4 md:flex-col lg:flex-row'
+                  >
+                    <p className='whitespace-nowrap'>{link.label}:</p>
+                    <p className='medium-14 whitespace-nowrap text-blue-70'>
+                      {link.value}
+                    </p>
+                  </Link>
+                ))}
+              </FooterColumn>
+            </div>
+            <div className='flex flex-col gap-5'>
+              <FooterColumn title={SOCIALS.title}>
+                <ul className='regular-14 flex gap-4 text-gray-30'>
+                  {SOCIALS.links.map((link) => (
+                    <Link href='/' key={link}>
+                      <Image
+                        src={`/assets/svg/socials/${link}.svg`}
+                        alt='brand-logos'
+                        width={24}
+                        height={24}
+                      />
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            </div>
+          </div>
+        </div>
+
+        <div className='border border-gray-600' />
+        <p className='regular-14 w-full text-center text-gray-600'>
+          &copy; 2023 ClassMate | All rights reserved
+        </p>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
--- app/components/index.ts
+++ app/components/index.ts
@@ -2,6 +2,7 @@ export { default as Navbar } from '@components/Navbar/Navbar'
 export { default as NavSidebar } from '@components/Navbar/NavSidebar'
 // @footer
 export { default as Footer } from '@components/Footer/Footer'
+export { default as FooterColumn } from '@components/Footer/FooterColumn'
 // @button
 export { default as Button } from '@components/Button/Button'
 // @about
