10f1bc9de6e1ab426b5eb9295097d5cd313381fe
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-16 09:07:14 +1300

    change in Sign In modal

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,8 @@ import { SignIn } from '@clerk/nextjs'
 
 const SignInPage = () => {
   return (
-    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
-      <div className='padding-container max-container w-full pb-24'>
+    <section className='max-container padding-container flex flex-col items-center justify-center gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
+      <div className='w-full pb-24'>
         <SignIn />
       </div>
     </section>
