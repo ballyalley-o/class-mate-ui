2f40e8328e7c472d1004b2bb2c3c837128d5d504
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-17 01:58:58 +1300

    🐎 ci(added auth middleware config):

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,24 @@
-import { authMiddleware } from '@clerk/nextjs'
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs'
+import { NextResponse } from 'next/server';
 
 export default authMiddleware({
   publicRoutes: ['/', '/about', '/register'],
-})
+  afterAuth(auth, req, evt) {
+      if (!auth.userId && !auth.isPublicRoute) {
+        return redirectToSignIn({ returnBackUrl: req.url });
+      }
+      if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/"){
+        const orgSelection = new URL('/', req.url)
+        return NextResponse.redirect(orgSelection)
+      }
+      if (auth.userId && !auth.isPublicRoute) {
+        return NextResponse.next()
+      }
+
+      return NextResponse.next();
+    }
+  }
+)
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
