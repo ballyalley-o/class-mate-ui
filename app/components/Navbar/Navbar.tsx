8b75f1a2c85a0feebcb79a49cbdcdfd4d7dfdfea
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-17 23:40:18 +1300

    dropdown for nav items

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,16 +1,19 @@
 'use client'
 
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, Fragment } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import * as _ from '@assets/styles/navbar'
 import NavBrand from '@components/Navbar/NavBrand'
+import { Menu, Transition } from '@headlessui/react'
 import { motion } from 'framer-motion'
 // @clerk
 import { UserButton, auth } from '@clerk/nextjs'
 import { Button, LinkWrap } from '@components'
 import { NAV } from '@constants'
 // @utils
+import classNames from 'classnames'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { delay } from '@utils'
 
 type Navbar = {
@@ -61,13 +64,91 @@ const Navbar: React.FC<Navbar> = ({ auth, actor }: Navbar) => {
       <NavBrand />
       <ul className='hidden h-full gap-12 lg:flex'>
         {NAV.map((link) => (
-          <Link
-            href={link.href}
-            key={link.key}
-            className='light-14 hover:regular-18 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all ease-in-out'
-          >
-            <motion.div className='w-20'>{link.label}</motion.div>
-          </Link>
+           <Menu as="div" className="relative inline-block text-left">
+           <div>
+             <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                {link.label}
+               <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+             </Menu.Button>
+           </div>
+
+           <Transition
+             as={Fragment}
+             enter="transition ease-out duration-100"
+             enterFrom="transform opacity-0 scale-95"
+             enterTo="transform opacity-100 scale-100"
+             leave="transition ease-in duration-75"
+             leaveFrom="transform opacity-100 scale-100"
+             leaveTo="transform opacity-0 scale-95"
+           >
+             <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+               <div className="py-1">
+                 <Menu.Item>
+                   {({ active }) => (
+                     <a
+                       href="#"
+                       className={classNames(
+                         active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                         'block px-4 py-2 text-sm'
+                       )}
+                     >
+                       Account settings
+                     </a>
+                   )}
+                 </Menu.Item>
+                 <Menu.Item>
+                   {({ active }) => (
+                     <a
+                       href="#"
+                       className={classNames(
+                         active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                         'block px-4 py-2 text-sm'
+                       )}
+                     >
+                       Support
+                     </a>
+                   )}
+                 </Menu.Item>
+                 <Menu.Item>
+                   {({ active }) => (
+                     <a
+                       href="#"
+                       className={classNames(
+                         active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                         'block px-4 py-2 text-sm'
+                       )}
+                     >
+                       License
+                     </a>
+                   )}
+                 </Menu.Item>
+                 <form method="POST" action="#">
+                   <Menu.Item>
+                     {({ active }) => (
+                       <button
+                         type="submit"
+                         className={classNames(
+                           active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                           'block w-full px-4 py-2 text-left text-sm'
+                         )}
+                       >
+                         Sign out
+                       </button>
+                     )}
+                   </Menu.Item>
+                 </form>
+               </div>
+             </Menu.Items>
+           </Transition>
+         </Menu>
+
+          // <Link
+          //   href={link.href}
+          //   key={link.key}
+          //   className='light-14 hover:regular-18 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all ease-in-out'
+          // >
+          //   <motion.div className='w-20'>{link.label}</motion.div>
+          // </Link>
         ))}
       </ul>
 
