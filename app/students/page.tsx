4ab589257219b3e5bb1f407ab0296da26a464dd7
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-18 03:27:17 +1300

    student page

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { StudentTable } from '@components'
+import { mockStudents } from '@constants/mock'
 
 const Students = () => {
-  return <div>Students</div>
+  const studentArray = [{}]
+
+  return (
+    <section className='flex-col flexCenter overflow-hidden bg-projects bg-center bg-no-repeat py-24'>
+      <div className='max-container padding-container relative w-full flex justify-end'>
+        <div className='flex flex-1 lg:min-h-[900px]'>
+          <StudentTable students={mockStudents} />
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Students
