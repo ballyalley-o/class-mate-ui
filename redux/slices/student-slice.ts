312c7e5464278a7e89eb1eea8fb332e8df654344
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-23 05:41:48 +1300

    student slice

---
+++ redux/slices/student-slice.ts
@@ -0,0 +1,12 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Student } from '@interfaces/Student'
+
+// type StudentState = {}
+
+const initialState = {}
+
+export const students = createSlice({
+  name: 'students',
+  initialState: '',
+  reducers: {},
+})
