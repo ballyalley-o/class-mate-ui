7d4ba66a71dd2af1da1e7c9a99dd3bc64bdd0477
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-24 19:12:51 +1000

    api route conastants

--- app/constants/routes/api.ts
+++ app/constants/routes/api.ts
@@ -1,10 +1,28 @@
 import path from 'path'
 import { GLOBAL } from '@config'
 
-const apiUrl = GLOBAL.api
+const apiUrl = GLOBAL.api || ''
+
+function apiConnect(param: string) {
+  if (!apiUrl) {
+    throw new Error('apiUrl is undefined')
+  }
+
+  const apiPath = path.join(apiUrl, param)
+
+  return apiPath
+}
 
 const API = {
-  students: apiUrl && path.join(apiUrl, '/student'),
+  students: apiConnect('/student'),
+  trainer: apiConnect('/trainer'),
+  cohort: apiConnect('/cohort'),
+  role: apiConnect('/role'),
+  modules: apiConnect('/module'),
+  // @auth
+  signIn: apiConnect('/sign-in'),
+  signUp: apiConnect('/sign-up'),
+  signOut: apiConnect('/sign-out'),
 }
 
 export default API
