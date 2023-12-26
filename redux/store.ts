95b621d98fb6e1cc71ca0428d3fd5de4647846d2
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-26 20:29:35 +1000

    mount api and students to store

--- redux/store.ts
+++ redux/store.ts
@@ -1,13 +1,18 @@
 // import studentReducer from '@redux/slices/student-slice'
 import { configureStore } from '@reduxjs/toolkit'
-import { studentReducer, student } from '@redux/slices'
+import { Action, ThunkAction } from '@reduxjs/toolkit'
+// import { studentReducer, student } from '@redux/slices'
+import { apiSlice } from '@redux/slices/api-slice'
 
 export const store = configureStore({
   reducer: {
-    studentReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: process.env.NODE_ENV !== 'production',
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+export type AppThunk = ThunkAction<void, RootState, null, Action<string>>
