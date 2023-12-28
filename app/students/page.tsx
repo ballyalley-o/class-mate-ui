11bc89ca18a5429dd78ffa2be894bb67e1233277
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-28 19:05:27 +1000

    error handler in students page

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -7,18 +7,30 @@ import { mockStudents } from '@constants/mock'
 import { useGetStudentsQuery } from '@lib/features/student-slice'
 
 const Students = () => {
-  const { data: students, isLoading, error } = useGetStudentsQuery()
+  const { data, isLoading, error } = useGetStudentsQuery()
+
+  if (error) {
+    // Check if it's a FetchBaseQueryError
+    if ('error' in error) {
+      // Handle FetchBaseQueryError
+      console.error('Error:', error.error)
+    } else {
+      // Handle SerializedError
+      console.error('Error:', error)
+    }
+
+    // Render your error component or message
+    return <div>Error occurred</div>
+  }
 
   return (
     <section className='flex-col flexCenter overflow-hidden bg-projects bg-center bg-no-repeat py-24'>
       {isLoading ? (
         <h1> Loading... </h1>
-      ) : error ? (
-        <div>{error?.data?.message || error?.error} </div>
       ) : (
         <div className='max-container padding-container relative w-full flex justify-end'>
           <div className='flex flex-1 lg:min-h-[900px]'>
-            <StudentTable students={students?.students} />
+            <StudentTable students={data.students} />
           </div>
         </div>
       )}
