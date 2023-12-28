f7af837330020002b1bd86234e3d5f33e9c70512
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-28 19:07:42 +1000

    imports and interface for students api endpoints

--- lib/features/student-slice.ts
+++ lib/features/student-slice.ts
@@ -1,5 +1,16 @@
+import {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+  FetchBaseQueryMeta,
+} from '@reduxjs/toolkit/query/react'
 import { API } from '@constants/routes'
-import { apiSlice } from './api-slice'
+import { apiSlice } from '@lib/features/api-slice'
+
+interface StudentsApiEndpoints {
+  getStudents: (arg?: FetchArgs) => { url: string }
+  [key: string]: (arg?: any) => any
+}
 
 export const studentsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
