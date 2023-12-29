8bd3cf3033c739e85c9a37a1007f988e15e4fa10
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-29 12:01:43 +1000

    student slice changes

--- lib/features/student-slice.ts
+++ lib/features/student-slice.ts
@@ -1,21 +1,20 @@
-import {
-  BaseQueryFn,
-  FetchArgs,
-  FetchBaseQueryError,
-  FetchBaseQueryMeta,
-} from '@reduxjs/toolkit/query/react'
-import { API } from '@constants/routes'
+import { FetchArgs } from '@reduxjs/toolkit/query/react'
+import { API } from '@app-routes'
 import { apiSlice } from '@lib/features/api-slice'
 
 interface StudentsApiEndpoints {
-  getStudents: (arg?: FetchArgs) => { url: string }
-  [key: string]: (arg?: any) => any
+  getStudents: () => { url: string }
+  [key: string]: () => any
 }
+
+// const apiUrl = 'http://localhost:3004/api/v0.1'
 
 export const studentsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getStudents: builder.query({
       query: () => ({
+        // url: apiUrl + '/student',
+        // FIXME: constants outside of module  doesn't work
         url: API.students,
       }),
       keepUnusedDataFor: 5,
