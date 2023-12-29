d3459151dbb982fbcf5c872dfe8d6de3c7578d82
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-29 15:03:21 +1000

    🐞 fix(fixed API path params):

--- lib/features/student-slice.ts
+++ lib/features/student-slice.ts
@@ -7,14 +7,10 @@ interface StudentsApiEndpoints {
   [key: string]: () => any
 }
 
-// const apiUrl = 'http://localhost:3004/api/v0.1'
-
 export const studentsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getStudents: builder.query({
       query: () => ({
-        // url: apiUrl + '/student',
-        // FIXME: constants outside of module  doesn't work
         url: API.students,
       }),
       keepUnusedDataFor: 5,
