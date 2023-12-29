6e3dcc7de4c550498145caa5c25a29d97476a652
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-29 12:00:59 +1000

    student pages changes

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -2,38 +2,41 @@
 
 import React from 'react'
 import { StudentTable } from '@components'
-import { mockStudents } from '@constants/mock'
+// @redux
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query/react'
+
 // @slices
 import { useGetStudentsQuery } from '@lib/features/student-slice'
 
 const Students = () => {
-  const { data, isLoading, error } = useGetStudentsQuery()
+  const { data, isLoading, error } = useGetStudentsQuery({})
 
-  if (error) {
-    // Check if it's a FetchBaseQueryError
-    if ('error' in error) {
-      // Handle FetchBaseQueryError
-      console.error('Error:', error.error)
-    } else {
-      // Handle SerializedError
-      console.error('Error:', error)
-    }
-
-    // Render your error component or message
-    return <div>Error occurred</div>
+  const isFetchBaseQueryError = (error: any): error is FetchBaseQueryError => {
+    return 'data' in error
   }
 
   return (
     <section className='flex-col flexCenter overflow-hidden bg-projects bg-center bg-no-repeat py-24'>
-      {isLoading ? (
-        <h1> Loading... </h1>
-      ) : (
-        <div className='max-container padding-container relative w-full flex justify-end'>
-          <div className='flex flex-1 lg:min-h-[900px]'>
+      <div className='max-container padding-container relative w-full flex justify-end'>
+        <div className='flex flex-1 lg:min-h-[900px]'>
+          {isLoading ? (
+            <div className='text-center m-auto'>
+              <h1>Loading...</h1>
+            </div>
+          ) : error ? (
+            <div className='text-center m-auto'>
+              <h1>
+                {isFetchBaseQueryError(error)
+                  ? (error as FetchBaseQueryError).data?.error ||
+                    'An error occurred'
+                  : String(error)}
+              </h1>
+            </div>
+          ) : (
             <StudentTable students={data.students} />
-          </div>
+          )}
         </div>
-      )}
+      </div>
     </section>
   )
 }
