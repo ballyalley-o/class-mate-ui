13ea5eef2db24a663c52720d3890dd9fda9a0cb3
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-12-29 16:33:30 +1000

    🦄 refactor(refactor and cleanup, student headers and classes):

--- app/components/Table/StudentTable.tsx
+++ app/components/Table/StudentTable.tsx
@@ -1,27 +1,18 @@
 import { StudentTableProps } from '@interfaces/Student'
+// @constants
+import { studentHeader } from '@constants'
 import { CheckCircleIcon } from '@heroicons/react/20/solid'
 import { XCircleIcon } from '@heroicons/react/20/solid'
 
 const StudentTable = ({ students }: StudentTableProps) => {
-  const headers = [
-    'Status',
-    'ID',
-    'First Name',
-    'Last Name',
-    'Email',
-    'Username',
-    'Location',
-    'Cohort',
-  ]
-
   return (
     <table className='min-w-full divide-y divide-gray-800 border-collapse'>
       <thead>
         <tr>
-          {headers.map((header) => (
+          {studentHeader.map((header) => (
             <th
               key={header}
-              className='py-2 px-4 bg-black font-bold text-gray-100 uppercase border-b'
+              className='px-4 bg-black font-bold text-gray-100 uppercase '
             >
               {header}
             </th>
@@ -30,27 +21,31 @@ const StudentTable = ({ students }: StudentTableProps) => {
       </thead>
       <tbody>
         {students.map((student) => (
-          <tr key={student?._id} className={'bg-black text-white'}>
-            <td className='px-6  border-b justify-center text-center'>
-              {student?.isActive ? (
-                <CheckCircleIcon
-                  width={15}
-                  height={15}
-                  className='text-green-500 my-auto'
-                />
-              ) : (
-                <XCircleIcon width={15} height={15} className='text-red-500' />
-              )}
+          <tr key={student?._id} className={'bg-black text-white border-b'}>
+            <td className=''>
+              <div className='inline-block'>
+                {student?.isActive ? (
+                  <CheckCircleIcon
+                    width={15}
+                    height={15}
+                    className='text-green-500 inline-block'
+                  />
+                ) : (
+                  <XCircleIcon
+                    width={15}
+                    height={15}
+                    className='text-red-500 inline-block'
+                  />
+                )}
+              </div>
             </td>
-            <td className='px-6 text-center border-b'>{student?._id}</td>
-            <td className='px-4 text-center border-b'>{student?.firstname}</td>
-            <td className='px-4 text-center border-b'>{student?.lastname}</td>
-            <td className='px-4 text-center border-b'>{student?.email}</td>
-            <td className='px-4 text-center border-b'>{student?.username}</td>
-            <td className='px-4 text-center border-b'>{student?.location}</td>
-            <td className='px-4 text-center border-b'>
-              {student?.cohort?.name}
-            </td>
+            <td>{student?._id}</td>
+            <td>{student?.firstname}</td>
+            <td>{student?.lastname}</td>
+            <td>{student?.email}</td>
+            <td>{student?.username}</td>
+            <td>{student?.location}</td>
+            <td>{student?.cohort?.name}</td>
           </tr>
         ))}
       </tbody>
