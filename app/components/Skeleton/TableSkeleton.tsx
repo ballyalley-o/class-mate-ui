b4eb5d3f3b2e3f0d682c6e8b2410835e3d187805
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-02 15:09:25 +1000

    🎉 init(table skeleton component):

---
+++ app/components/Skeleton/TableSkeleton.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react'
+
+interface TableSkeletonProps {
+  numRows: number
+}
+
+const TableSkeleton: FC<TableSkeletonProps> = ({ numRows }) => {
+  const skeletonRows = Array.from({ length: numRows }, (_, rowIndex) => (
+    <div
+      key={rowIndex}
+      className='h-5 bg-gray-200  mt-4  dark:bg-gray-900 w-full mb-4'
+    ></div>
+  ))
+
+  return (
+    <div
+      role='status'
+      className='space-y-8 animate-pulse md:space-x-10 rtl:space-x-reverse md:flex md:items-center'
+    >
+      <div className='w-full gap-5'>{skeletonRows}</div>
+      <span className='sr-only'>Loading...</span>
+    </div>
+  )
+}
+
+export default TableSkeleton
