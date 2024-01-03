73f7447dc241f0eaef23d2b7c30c673fb7d31c6a
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-04 00:16:35 +1000

    🦄 refactor(icon constants):

--- app/assets/icons/icon.ts
+++ app/assets/icons/icon.ts
@@ -1,8 +1,25 @@
-import { CheckCircleIcon } from '@heroicons/react/20/solid'
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/20/solid'
+import { IconConstant } from '@interfaces/Consants'
+// @styles
+import { icon } from '@assets/styles'
 
-const ICON = {
-  nav: {},
-  checkIcon: CheckCircleIcon,
+const ICON: IconConstant = {
+  active: {
+    icon: CheckCircleIcon,
+    props: {
+      className: icon.StyledActiveIcon,
+      width: 15,
+      height: 15,
+    },
+  },
+  inActive: {
+    icon: XCircleIcon,
+    props: {
+      className: icon.StyledInActiveIcon,
+      width: 15,
+      height: 15,
+    },
+  },
 }
 
 export default ICON
