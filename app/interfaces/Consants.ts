84af2cf33c044937eaf276a924a8f2a6eaa97dc4
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-05 02:17:01 +1000

    constant interface

--- app/interfaces/Consants.ts
+++ app/interfaces/Consants.ts
@@ -1,7 +1,11 @@
+import { StaticImport } from 'next/dist/shared/lib/get-img-props'
+
 export interface IconProps {
+  src?: string | StaticImport
   className?: string
   width?: string | number
   height?: string | number
+  alt: string
 }
 
 export interface IconConstant {
