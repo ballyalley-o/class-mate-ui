1fe781ebf13795a23027bccff5159bf42c9153ff
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-01-05 23:08:35 +1000

    🦄 refactor(search input styled):

--- app/assets/styles/default.ts
+++ app/assets/styles/default.ts
@@ -1,3 +1,19 @@
 import classNames from 'classnames'
 
 export const StyledDashboardH1 = classNames('hero-title-daashbord text-8xl')
+export const StyledTopRightContainer = classNames(
+  'flex flex-end justify-end container'
+)
+
+// @search-input
+export const StyledSearchInput = classNames(
+  'block pt-2 ps-10 text-sm text-gray-900/60 w-80 bg-gray-50 dark:bg-gray-900/60 dark:placeholder-gray-400 dark:text-white'
+)
+export const StyledTopRightWrapper = classNames(
+  'md:flex-column space-y-4 md:space-y-0 py-4'
+)
+export const StyledSearchInputLabel = classNames('sr-only')
+export const StyledSearchInputBase = classNames('relative')
+export const StyledSearchInputBaseWrapper = classNames(
+  'absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none'
+)
